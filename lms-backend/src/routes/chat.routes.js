f885c41ed73cd8b62a30208a975cec689437f514
Trịnh Ngọc Thái <94618886+banhda123@users.noreply.github.com>
2026-03-28 23:09:51 +0700

    fix

--- lms-backend/src/routes/chat.routes.js
+++ lms-backend/src/routes/chat.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authMiddleware } = require('../middlewares/auth');
+const authMiddleware = require('../middlewares/auth');
 const lessonChatController = require('../modules/chat/lessonChat.controller');
 const {
   getChatValidation,
